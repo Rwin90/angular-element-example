5520fcebf682b847376d036804660cd134a15851
a.ranjbar <a.ranjbar@fakour.net>
2019-07-01 10:15:38 +0430

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
 import { AppComponent } from './app.component';
+import { MessageComponent } from './msg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [],
+  entryComponents: [MessageComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+
+   
+
+   
+  }
+  ngDoBootstrap() {
+    const customElement = createCustomElement(MessageComponent, { injector: this.injector });
+    customElements.define('app-message', customElement);
+  }
+}
